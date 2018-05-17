cb5bb3454e3a32f3b3987ee65c5a392078f3307a
notjamesheck <224@holbertonschool.com>
2018-05-16 23:59:07 -0700

    thanks medium

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # phpp to php
 exec { '/var/www/html/wp-settings.php':  
-path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-cwd     => '/var/www/html/wp-includes',  
-command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
+path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  
+command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
