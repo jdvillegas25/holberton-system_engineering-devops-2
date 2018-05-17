70768026ef966e9a145ee5a82575172d963acc12
notjamesheck <224@holbertonschool.com>
2018-05-17 06:43:29 +0000

    mv order

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # phpp to php
 exec { 'fix file typo':  
 path    => '/usr/bin:/bin',  
-command => 'mv class-wp-locale.php class-wp-locale.phpp',
+command => 'mv class-wp-locale.phpp class-wp-locale.php',
 }
