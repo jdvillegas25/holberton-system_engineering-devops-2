c8ee71c68f00f232352f806aa5dd7cb98d65376e
notjamesheck <224@holbertonschool.com>
2018-05-17 11:16:12 -0700

    syntax puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# path -> path used for command execution...
-# sed -i 's/original/new/g' file.txt
-exec { ‘just-work’:
-  path => ‘usr/bin/:/bin/’,
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+# path ->, sed -i 's/original/new/g' file.txt
+exec { ‘fix-phpp’:
+  path     => 'usr/bin/:/bin/',
+  command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  provider => 'shell',
 }
