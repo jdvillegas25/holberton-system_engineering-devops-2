35ba6707994c29c92a2a312eabd829dc6fc614b5
notjamesheck <224@holbertonschool.com>
2018-05-17 11:53:02 -0700

    do it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # path ->, sed -i 's/original/new/g' file.txt
-exec { ‘fix-phpp’:
+exec { 'fix-phpp':
   path     => 'usr/bin/:/bin/',
   command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   provider => 'shell',
