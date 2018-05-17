1e1bbd6c2545ee541c708c8318815fb5865480b9
notjamesheck <224@holbertonschool.com>
2018-05-16 23:48:24 -0700

    add dir

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # phpp to php
 exec { 'fix file typo':  
-path    => '/usr/bin:/bin',  
+path    => '/usr/bin:/bin',
+cwd     => '/var/www/html/wp-includes',  
 command => 'mv class-wp-locale.phpp class-wp-locale.php',
 }
