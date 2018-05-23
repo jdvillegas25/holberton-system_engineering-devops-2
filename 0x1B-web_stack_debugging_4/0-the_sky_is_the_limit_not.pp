1a28376e5076be51661fb4f704ae1aaf97e54030
notjamesheck <224@holbertonschool.com>
2018-05-23 19:32:29 +0000

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
 # path -> sed -i 's/original/new/g' -> file
 exec { 'fix-ulimit':
   path     => 'usr/bin/:/bin/',
-  command  => "sed -i -e 's/15/4096/g' /etc/default/nginx",
+  command  => 'sed -i -e "s/15/4096/g" /etc/default/nginx',
   provider => 'shell',
+  notify   => Exec['restart-nginx'],
 }
 
 exec { 'restart-nginx':
   command  => 'sudo service nginx restart',
   provider => 'shell',
+  require  => Exec['fix-ulimit'],
 }
